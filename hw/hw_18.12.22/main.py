4552ccc374267ecb0d83d3541efbdfac9c09c120
amer1_can_boy <serzh.plichko@mail.ru>
2022-12-19 19:53:37 +0300

    put templates in directory "templates"

--- hw/hw_18.12.22/main.py
+++ hw/hw_18.12.22/main.py
@@ -3,13 +3,17 @@ import utils
 
 app = Flask(__name__)
 
+pk = ""
+candidate_name = ''
+skill = ''
+
 
 @app.route("/")
 def index():
     candidates = utils.get_all_candidates()
     return render_template('list.html', candidates=candidates)
 
-@app.route("/candidate/<int:pk>")
+@app.route("/candidates/<int:pk>")
 def get_candidate(pk):
     candidate = utils.get_candidate_by_pk(pk)
     return render_template("card.html", candidate = candidate)
@@ -23,7 +27,6 @@ def get_candidate_by_name(candidate_name):
 def get_candidate_by_skills(skill):
     candidates = utils.get_candidates_by_skill(skill)
     return render_template("skill.html", candidates = candidates, count_candidates = len(candidates))
-
 
 
 app.run(debug=True)
